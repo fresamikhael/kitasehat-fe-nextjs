e87c818b77ba7ce5ba9f2b662339542685531f91
fresaletlora <mikhaelfresa@gmail.com>
2022-04-08 15:19:05 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -123,12 +123,7 @@ export default function index() {
         <section id="about" className="about">
           <div className="container-fluid">
             <div className="row">
-              <div className="col-xl-5 col-lg-6 video-box d-flex justify-content-center align-items-stretch position-relative">
-                <a
-                  href="https://www.youtube.com/watch?v=jDDaplaOz7Q"
-                  className="glightbox play-btn mb-4"
-                ></a>
-              </div>
+              <div className="col-xl-5 col-lg-6 video-box d-flex justify-content-center align-items-stretch position-relative"></div>
 
               <div className="col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5">
                 <h3>Enim quis est voluptatibus aliquid consequatur fugiat</h3>
